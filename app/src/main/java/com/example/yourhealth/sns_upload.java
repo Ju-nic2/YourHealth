80dddcdcb00adc60e201a2e5308cc9374e5a1996
sangyun884 <71681194+sangyun884@users.noreply.github.com>
2020-11-10 03:25:24 +0900

    sns_upload layout implemented

---
+++ app/src/main/java/com/example/yourhealth/sns_upload.java
@@ -0,0 +1,14 @@
+package com.example.yourhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sns_upload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sns_upload);
+    }
+}
