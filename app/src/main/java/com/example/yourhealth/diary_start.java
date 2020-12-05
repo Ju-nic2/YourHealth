7c6be28a7491833561cfa3020a461eb9c29352b7
HyungDamShin <tlsgudeka@gmail.com>
2020-12-05 17:10:31 +0900

    update

--- app/src/main/java/com/example/yourhealth/diary_start.java
+++ app/src/main/java/com/example/yourhealth/diary_start.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class diary_start extends AppCompatActivity {
 
@@ -27,15 +28,23 @@ public class diary_start extends AppCompatActivity {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 date = year + "/" + (month + 1) + "/" + dayOfMonth;
+                writeDiaryBtn.setText(date + " 운동일지 작성");
             }
         });
 
         writeDiaryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), diary_main.class);
-                intent.putExtra("cur_date", date);
-                startActivity(intent);
+                if(date != null)
+                {
+                    Intent intent = new Intent(getApplicationContext(), diary_main.class);
+                    intent.putExtra("cur_date", date);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(diary_start.this, "날짜를 선택해주세요.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
