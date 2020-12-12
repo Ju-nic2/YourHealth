a6ab5f7f5817121b728d25e0e5fd3b8074f48b6c
HyungDamShin <tlsgudeka@gmail.com>
2020-12-12 17:13:38 +0900

    new

---
+++ app/src/main/java/com/example/yourhealth/storage_saved_data.java
@@ -0,0 +1,14 @@
+package com.example.yourhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class storage_saved_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage_saved_data);
+    }
+}
